d2bed62db507b8d894d34ac6ed7d04d62436bbb7
Christer Wendel <christer.wendel@altrusoft.se>
2014-05-09 14:07:24 +0200

    Removing files created by nagios3-common

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -6,6 +6,13 @@ class nagiosserver::configure {
   $services = $nagiosserver::services
   $conf_dir = $nagiosserver::conf_dir
 
+  file { ['/etc/nagios3/conf.d/extinfo_nagios2.cfg',
+          '/etc/nagios3/conf.d/hostgroups_nagios2.cfg',
+          '/etc/nagios3/conf.d/localhost_nagios2.cfg',
+          '/etc/nagios3/conf.d/services_nagios2.cfg',]:
+            ensure => absent,
+  }
+
   nagiosserver::add_nagios_host{ $hosts: }
 
   nagiosserver::add_nagios_service{ $services: }
